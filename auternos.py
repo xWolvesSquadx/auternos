49c98f0cb241f8f5997c9530b92644ddbd2b3ed5
Benji Silver <ddomino007@gmail.com>
2021-12-09 06:02:20 +0100

    add captcha bypass and click of cookie prompt
    Slow down login process a bit + force remove some args, this way aternos doesn't know it's automated.
    Also autoaccept the cookie prompt.

--- auternos.py
+++ auternos.py
@@ -3,25 +3,32 @@ import time
 import sys
 import os
 
-driver = webdriver.Chrome('/path/to/bin/chromedriver')
-options = webdriver.ChromeOptions()
+options = Options()
+option = webdriver.ChromeOptions()
+option.add_experimental_option("excludeSwitches", ["enable-automation"])
+option.add_experimental_option('useAutomationExtension', False)
+option.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-options.binary_location = "/path/to/bin/chromium"
-driver = webdriver.Chrome(options=options)
-driver.get('https://aternos.org/servers/')
+driver = webdriver.Chrome(executable_path=r'/path/to/bin/chromedriver',options=option)
+driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+driver.get('https://aternos.org/server')
 
 # log in
 text_area = driver.find_element_by_id('user')
-text_area.send_keys("username")
-
+text_area.send_keys("login")
+time.sleep(3)
 text_area = driver.find_element_by_id('password')
 text_area.send_keys("password")
-
+time.sleep(3)
 login = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[4]/div[3]/div[4]')
 login.click()
-
 time.sleep(3)
+driver.get('https://aternos.org/server')
+time.sleep(3)
+
+# accept cookie privacy prompt
+cookieaccept = driver.find_element_by_xpath('/html/body/div[3]/div/div/div/div[3]/div[2]/div[2]')
+cookieaccept.click()
 
 # select server
 select = driver.find_element_by_xpath('/html/body/div/main/section/div/div[2]/div/div[1]')
